cfdf01649dd595df7825414d1567480c44d57c81
Julio <jafernandezg9@gmail.com>
2022-02-08 15:54:08 +0100

    Added logs in Exception

--- App/src/main/java/com/jafernandez/operations/exceptions/ExceptionHandler.java
+++ App/src/main/java/com/jafernandez/operations/exceptions/ExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.jafernandez.operations.exceptions;
 
 import com.jafernandez.operations.dto.ErrorResponseDto;
+import io.corp.calculator.TracerImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,8 @@ import java.math.BigDecimal;
 @ControllerAdvice
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
+  private io.corp.calculator.TracerImpl tracerApi = new TracerImpl();
+
   @org.springframework.web.bind.annotation.ExceptionHandler(InvalidOperationTypeException.class)
   public ResponseEntity<ErrorResponseDto> invalidOperationTypeException(InvalidOperationTypeException e) {
 
@@ -18,6 +21,8 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
     errorResponseDto.setCode(BigDecimal.valueOf(HttpStatus.BAD_REQUEST.value()));
     errorResponseDto.setDescription(e.getMessage());
+
+    tracerApi.trace(String.format("Error with code %s and description %s", errorResponseDto.getCode(), errorResponseDto.getDescription()));
 
     return ResponseEntity
             .badRequest()
@@ -32,6 +37,8 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
     errorResponseDto.setCode(BigDecimal.valueOf(HttpStatus.BAD_REQUEST.value()));
     errorResponseDto.setDescription(e.getMessage());
 
+    tracerApi.trace(String.format("Error with code %s and description %s", errorResponseDto.getCode(), errorResponseDto.getDescription()));
+
     return ResponseEntity
             .badRequest()
             .body(errorResponseDto);
@@ -44,6 +51,8 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
     errorResponseDto.setCode(BigDecimal.valueOf(HttpStatus.BAD_REQUEST.value()));
     errorResponseDto.setDescription("This is a bad argument.");
+
+    tracerApi.trace(String.format("Error with code %s and description %s", errorResponseDto.getCode(), errorResponseDto.getDescription()));
 
     return ResponseEntity
             .badRequest()
